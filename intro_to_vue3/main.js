5ecb9581d5a91396c7d2477e7c4287d181cad538
Viet Le <vietleuit@gmail.com>
2024-08-05 03:08:42 +0700

    Done: Part 4 - conditional rendering

--- intro_to_vue3/main.js
+++ intro_to_vue3/main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://www.google.com'
+            inventory: 11,
+            onSale: true
         }
     }
 })
