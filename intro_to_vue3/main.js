40e63d81db7b88479e39265989a4f4d258f9653e
Viet Le <vietleuit@gmail.com>
2024-08-05 04:53:56 +0700

    Done: Part 8 - computed properties

--- intro_to_vue3/main.js
+++ intro_to_vue3/main.js
@@ -1,14 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
+            brand: 'Jouney To Conquer Vue',
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inventory: 0,
+            selectedVariant: 0,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quanity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quanity: 0 }
             ],
             sizes: ['S', 'M', 'L', 'XL'],
             cart: 0
@@ -18,8 +18,9 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+
         },
         removeFromCart() {
             this.cart -= 1
@@ -27,7 +28,16 @@ const app = Vue.createApp({
     },
     computed: {
         isOutOfStock() {
-            return this.inventory === 0
+            return this.variants[this.selectedVariant].quanity === 0
+        },
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quanity
         }
     }
 })
